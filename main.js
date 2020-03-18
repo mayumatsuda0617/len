0b75ad3cfb3b52110e34e55679848b2d59bc076e
mayu matsuda <mayu.matsuda0617@gmail.com>
2020-03-18 17:34:15 +0900

    js commit:: Please enter the commit message for your changes. Lines starting

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+console.log('hello world');
+
+
+let a = 5;
+
+if (a > 10) {
+	console.log('a‚Í10‚æ‚è¬‚³‚¢');
+} else {
+	console.log('a ‚Í 10‚æ‚è‘å‚«‚¢');
+}
+
+for (let i = 0; i <50; i++) {
+	console.log(i);
+}
